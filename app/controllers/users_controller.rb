df653b10595fb4d946327de4c96767e697a4caef
Dee Race <deerace12@gmail.com>
2018-07-10 20:33:22 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :nausea, :happiness, :height, :tickets)
+    params.require(:user).permit(:name, :password, :nausea, :happiness, :height, :tickets, :admin)
   end
 
 
